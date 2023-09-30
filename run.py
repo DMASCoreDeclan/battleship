b7812fc816eb63401a4415e7042fbcdf79f51780
DMASCoreDeclan <declan.sweeney@gmail.com>
2023-09-30 11:28:15 +0100

    Update code to comply with CI Python Linter

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import os, math  # for clear()
+import os  # for clear()
 from time import sleep  # delay time before # clear() is used
 from random import randint  # for generating random numbers
 
@@ -9,15 +9,16 @@ grid_size = 0
 
 def clear():
     """
-    clears the sreen so that it doesn't become too crowded with previous interactions
+    clears the sreen so that it doesn't become too crowded with previous
+    interactions
     """
     os.system("clear")
 
 
 def welcome():
     """
-    This message will appear whenever the game starts or restarts with a new player,
-    it explains how the game works.
+    This message will appear whenever the game starts or restarts with a new
+    player, it explains how the game works.
     * denotes a HIT
     X denotes a MISS from a previous guess
     | | decontes a potential space for a guess
@@ -26,18 +27,18 @@ def welcome():
     clear()
     print("Welcome to Battleships\n")
     print(
-        "This game is designed to allow you to select the grid size you want to play in,\
-          \nthe number of ships you want the computer to place in the grid and determine\
-          \nthe number of bombs you want to be able to use.\
-          \nThe grid size is a square and can be between 4x4 and 10x10 in size.\
-          \nAfter each guess, the board with results of your previous guesses will be displayed and\
-          \nthe number of bombs you have remaining.\
-          \nAt the end of the game you will be asked if you want to play again or \
-          \nreturn to the main menu.\
-          \n* - Hit\
-          \nX - Miss\
-          \n| | Available as a target\
-          \n"
+        "This game is designed to allow you to select the grid size you want\
+            \nto play in, the number of ships you want the computer to place\
+            \nin the grid and determine the number of bombs you want to be\
+            \nable to use.  The grid size is a square and can be between 4x4\
+            \n and 10x10 in size.  After each guess, the board with results\
+            \nof your previous guesses will be displayed and the number of\
+            \nbombs you have remaining. At the end of the game you will be\
+            \n asked if you want to play again or return to the main menu.\
+            \n* - Hit\
+            \nX - Miss\
+            \n| | Available as a target\
+            \n"
     )
     sleep(1)
     # clear()
@@ -61,8 +62,9 @@ def new_player():
 
 def get_grid_size(name):
     """
-    Validates user input so that the choices they make for the grid_size are within the boundaries
-    of the program.  Asks the user to enter new inputs until they are acceptable.
+    Validates user input so that the choices they make for the grid_size are
+    within the boundaries of the program.  Asks the user to enter new inputs
+    until they are acceptable.
     Returns a validated GRID_SIZE.
     """
     while True:
@@ -84,8 +86,9 @@ def get_grid_size(name):
 
 def get_ships_value(name):
     """
-    Validates user input so that the choices they make for the number of ships are within
-    the boundaries of the program.  Asks the user to enter new inputs until they are acceptable.
+    Validates user input so that the choices they make for the number of
+    ships are within the boundaries of the program.  Asks the user to enter
+    new inputs until they are acceptable.
     Returns a valid number of SHIPS
     """
     while True:
@@ -104,12 +107,13 @@ def get_ships_value(name):
 
 def get_bomb_value(name, grid_size):
     """
-    Validates user input so that the choices they make for the nu,ber of bombs are within the
-    boundaries of the program.  Asks the user to enter new inputs until they are acceptable.
-    Returns a calculated number of BOMBS.  The calculation multiplies the GRID_SIZE by itself
-    giving the total possible number of locations where the SHIPS might be.  Depending on how
-    adventurous the user feels, they can decide what percentage of possible locations they can
-    shoot at, before the game begins.  The minimum value is 5% and the maximum value is 50%.
+    Validates user input so that the choices they make for the nu,ber of bombs
+    are within the boundaries of the program.  Asks the user to enter new
+    inputs until they are acceptable.  Returns a calculated number of BOMBS.
+    The calculation multiplies the GRID_SIZE by itself giving the total
+    possible number of locations where the SHIPS might be.  Depending on how
+    adventurous the user feels, they can decide whether they want the game to
+    be EASY, DIFFICULT or IMPOSSIBLE.
     """
     while True:
 
@@ -117,12 +121,18 @@ def get_bomb_value(name, grid_size):
             clear()
             bombs = input(
                 f"{name}, what level of difficulty are you looking for?\
-                          \nEasy gives you {(grid_size * grid_size) - 1} of the {(grid_size * grid_size)} bombs required\
-                          \nDifficult gives you 75% or {round((grid_size * grid_size) * .75)} of the bombs you require\
-                          \nImpossible gives you 50% or {round((grid_size * grid_size) * .5)} of the bombs you require\
-                          \nPress E for Easy\
-                          \nPress D for Difficult\
-                          \nPress I for Impossible\n"
+                \nEasy gives you {(grid_size * grid_size) - 1} of the \
+                {(grid_size * grid_size)} bombs required\
+                \nDifficult gives you 75% or \
+                {round((grid_size * grid_size) * .75)} \
+                of the bombs you require\
+                \nImpossible gives you 50% or \
+                {round((grid_size * grid_size) * .5)}\
+                 of the bombs you require\
+                \nPress E for Easy\
+                \nPress D for Difficult\
+                \nPress I for Impossible\
+                \nThe default is: Impossible"
             ).upper()
             if bombs in "EDI":
                 if bombs == "E":
@@ -160,9 +170,9 @@ def new_game():
     computer_board = prepare_computer_board(grid_size)
     previous_guess_board = prepare_guess_board(grid_size)
     print(
-        f"Thank you for your patience {name}\n\
+        f"Thank you for your patience {name}\
           \nYou have chosen to play with a grid: {grid_size} x {grid_size} in size with {ships} ships and {bombs} bombs.\
-          \nPreparing your game . . "
+          \nPreparing your game . . ."
     )
     sleep(5)
     return grid_size, ships, bombs, computer_board, previous_guess_board
@@ -170,11 +180,12 @@ def new_game():
 
 def draw_board(board, grid_size):
     """
-    This function requires 2 inputs: a BOARD type and the previously supplied GRID_SIZE.
-    It then draws a board with letters as headings and numbers as row values.  The user
-    will use the Letter and Number input to determine where they would like to place their
-    next guess.
-    Returns HEADING1 which is the list of valid letters that the user must choose fom later.
+    This function requires 2 inputs: a BOARD type and the previously supplied
+    GRID_SIZE.  It then draws a board with letters as headings and numbers as#
+    row values.  The user will use the Letter and Number input to determine
+    where they would like to place their next guess.
+    Returns HEADING1 which is the list of valid letters that the user must
+    choose fom later.
     """
     clear()
     heading1 = "  A B C D E F G H I J "  # Heading for Grid
@@ -184,11 +195,13 @@ def draw_board(board, grid_size):
     ) + 2  # There are 2 leading spaces and each row requires 2 spaces
     print(heading1[:heading_value])
     print(heading2[:heading_value])
-    grid_row = 0  #  begins the row numbering at 0
+    grid_row = 0  # begins the row numbering at 0
     for row in board:
         print(
             "%d|%s|" % (grid_row, "|".join(row))
-        )  # %s and %d are placeholders for a string and a number respectively. %s returns the string and %d returns a number.  The values are passed using % operator
+        )  # %s and %d are placeholders for a string and a number
+        # respectively. %s returns the string and %d returns a
+        # number.  The values are passed using % operator
         grid_row += 1
     return heading1[:heading_value]
 
@@ -197,9 +210,10 @@ def place_ships(board, grid_size, ships):
     """
     identifies a location for the computer to locate their SHIPS based on the
     user supplied GRID_SIZE and number of SHIPS.  The for an while keep going
-    until the correct number of SHIPS have been placed in the correct location
-    of the user defined GRID_SIZE.
-    Return a populated board with *s according to the input in get_user_preferences
+    until the correct number of SHIPS have been placed in the correct
+    location of the user defined GRID_SIZE.
+    Return a populated board with *s according to the input in
+    GET_USER_PREEFERENCES
     """
     for ship in range(ships):
         ship_row = randint(0, (grid_size - 1))
@@ -224,8 +238,9 @@ def prepare_boards():
 
 def get_letter(heading_value):
     """
-    Validates user input so that the choice they make for the letter of the column exists in the grid.
-    The letter is converted to a number from the LETTER_MAP dictionary.
+    Validates user input so that the choice they make for the letter of
+    the column exists in the grid.  The letter is converted to a number
+    from the LETTER_MAP dictionary.
     Returns a letter input as a 0 based index number.
     """
     letter_map = {
@@ -252,9 +267,11 @@ def get_letter(heading_value):
         except KeyError:
             print("Thats not a valid input")
 
+
 def get_number(grid_size):
     """
-    Validates user input so that the choice they make for the number of the row exists in the grid.
+    Validates user input so that the choice they make for the number of the
+    row exists in the grid.
     Returns a 0 based index number.
     """
     while True:
@@ -266,25 +283,28 @@ def get_number(grid_size):
                 return ship_row
             else:
                 print("Thats not a valid number")
-        except KeyError:
+        except (KeyError, ValueError) as error:
             print("Thats not a valid number")
+
 
 def request_guess(heading_value, grid_size):
     """
     Requests user input for a letter and a number to identify where the user
     wants to place their bomb.  The input is validated against the limitations
-    of the parameters that they chose in get_user_preferences.  If its not a valid
-    input, they are requested for the information until it is valid.
+    of the parameters that they chose in get_user_preferences.  If its not a
+    valid input, they are requested for the information until it is valid.
     Returns a valid set of 0 based numbers within the GRID_SIZE
     """
     ship_column = get_letter(heading_value)
     ship_row = get_number(grid_size)
     return ship_row, ship_column
 
+
 def get_heading_value():
     heading_value = draw_board(previous_guess_board, grid_size)
     heading_value = heading_value.replace(" ", "")
     return heading_value
+
 
 def count_hit_ships():
     hits = 0
@@ -293,6 +313,7 @@ def count_hit_ships():
             if column == "*":
                 hits += 1
     return hits
+
 
 def validate_guess(ship_row, ship_column, bombs, ships):
     clear()
@@ -321,10 +342,10 @@ def main(bombs, ships, grid_size):
     while bombs > 0 and count_hit_ships() != ships:
         print(f"declan has {bombs} bombs")
         ship_row, ship_column = request_guess(get_heading_value(), grid_size)
-        print(ship_row, ship_column)
         bombs = validate_guess(ship_row, ship_column, bombs, ships)
         print(
-            f"You have hit {count_hit_ships()} ships and have {ships - count_hit_ships()} ships left to hit."
+            f"You have hit {count_hit_ships()} ships and have \
+                {ships - count_hit_ships()} ships left to hit."
         )
         if bombs == 0:
             print("You have no bombs left!")
@@ -334,7 +355,7 @@ def main(bombs, ships, grid_size):
             print(f"You still have {bombs} bombs")
 
 
-while game_on == True:
+while game_on:
     if name == "":
         welcome()
         get_player_name()
@@ -354,4 +375,3 @@ while game_on == True:
         game_on = False
     else:
         name = ""
-
